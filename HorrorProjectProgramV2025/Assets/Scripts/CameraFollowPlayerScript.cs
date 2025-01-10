6e37bb9b79d91fb237866733de6f7770a09c1e6b
FedericoQB <federico.quijano@outlook.com>
2025-01-10 14:27:10 +0100

    Added checkmark to journal and font

--- HorrorProjectProgramV2025/Assets/Scripts/CameraFollowPlayerScript.cs
+++ HorrorProjectProgramV2025/Assets/Scripts/CameraFollowPlayerScript.cs
@@ -11,23 +11,28 @@ public class CameraFollowPlayerScript : MonoBehaviour
     public GameObject player;
     void Start()
     {
-        
+        if (player.transform.position.x > negativeXBarrier || player.transform.position.x < positiveXBarrier)
+        {
+            isMovable = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < positiveXBarrier && transform.position.x > negativeXBarrier)
+        if (transform.position.x < positiveXBarrier && transform.position.x > negativeXBarrier && isMovable == true)
         {
             transform.position = new Vector3(player.transform.position.x, 0.3f, -10);
         }
-        else if (transform.position.x > positiveXBarrier)
+        else if (player.transform.position.x > positiveXBarrier || player.transform.position.x < negativeXBarrier)
         {
-            transform.position = new Vector3(positiveXBarrier - 0.1f, 0.3f, -10);
-        }
-        else if (transform.position.x < negativeXBarrier)
-        {
-            transform.position = new Vector3(negativeXBarrier + 0.1f, 0.3f, -10);
+            isMovable = false;
+
+            if (player.transform.position.x > negativeXBarrier || player.transform.position.x < positiveXBarrier)
+            {
+                isMovable = true;
+                transform.position = new Vector3(player.transform.position.x, 0.3f, -10);
+            }
         }
 
         /*
