04693100dde3b4f20ee6ddd97727bdb06b051087
Febasco <ilja.purgin@elev.ga.lbs.se>
2025-01-10 11:10:00 +0100

    Music Trigger Fixed

--- HorrorProjectProgramV2025/Assets/MusicZone.cs
+++ HorrorProjectProgramV2025/Assets/MusicZone.cs
@@ -7,14 +7,20 @@ public class MusicZone : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        print("inne i trigger ej 2d");
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print("inne i trigger");
         // Check if the player enters the area
         if (other.CompareTag("Player"))
         {
+            print("inne i player trigger");
             SoundManager.Instance.PlayMusic(zoneMusic);
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit2D(Collider2D other)
     {
         // Stop the music when the player exits the area (optional)
         if (other.CompareTag("Player"))
