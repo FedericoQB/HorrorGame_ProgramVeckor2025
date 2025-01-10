d9fbdf94f5573241472029d6c13d97b6b243e985
FedericoQB <federico.quijano@outlook.com>
2025-01-10 15:16:19 +0100

    Fixed Camera and added main menu

---
+++ HorrorProjectProgramV2025/Assets/SceneManagerScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void EndApplication()
+    {
+        // Check if this works
+        Application.Quit();
+    }
+}
--- HorrorProjectProgramV2025/Assets/Scripts/CameraFollowPlayerScript.cs
+++ HorrorProjectProgramV2025/Assets/Scripts/CameraFollowPlayerScript.cs
@@ -11,7 +11,7 @@ public class CameraFollowPlayerScript : MonoBehaviour
     public GameObject player;
     void Start()
     {
-        if (player.transform.position.x > negativeXBarrier || player.transform.position.x < positiveXBarrier)
+        if (player.transform.position.x > negativeXBarrier && player.transform.position.x < positiveXBarrier)
         {
             isMovable = true;
         }
@@ -20,30 +20,19 @@ public class CameraFollowPlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < positiveXBarrier && transform.position.x > negativeXBarrier && isMovable == true)
+        if (isMovable == true)
         {
             transform.position = new Vector3(player.transform.position.x, 0.3f, -10);
         }
-        else if (player.transform.position.x > positiveXBarrier || player.transform.position.x < negativeXBarrier)
+
+        if (player.transform.position.x > positiveXBarrier || player.transform.position.x < negativeXBarrier)
         {
             isMovable = false;
-
-            if (player.transform.position.x > negativeXBarrier || player.transform.position.x < positiveXBarrier)
-            {
-                isMovable = true;
-                transform.position = new Vector3(player.transform.position.x, 0.3f, -10);
-            }
         }
 
-        /*
-        if (player.transform.position.x <= negativeXBarrier)
+        if (player.transform.position.x > negativeXBarrier && player.transform.position.x < positiveXBarrier)
         {
-            transform.position = new Vector3(player.transform.position.x, 0.3f, -10);
+            isMovable = true;
         }
-        else if (player.transform.position.x >= positiveXBarrier)
-        {
-            transform.position = new Vector3(player.transform.position.x, 0.3f, -10);
-        }
-        */
     }
 }
