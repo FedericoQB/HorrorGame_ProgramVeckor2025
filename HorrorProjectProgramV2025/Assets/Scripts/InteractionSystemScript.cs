6ca162e1017c13bc58013ebaf5b2d2e26974fcd3
FedericoQB <federico.quijano@outlook.com>
2025-01-10 09:18:57 +0100

    Added JournalScript and Interaction Script

--- HorrorProjectProgramV2025/Assets/Scripts/InteractionSystemScript.cs
+++ HorrorProjectProgramV2025/Assets/Scripts/InteractionSystemScript.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class InteractionSystemScript : MonoBehaviour
 {
-    public GameObject journalCanvas;
-    bool journalOpen = false;
+    bool isPlayerNear = false;
+    public bool isAbleToPickUp = false;
+    public bool isADoor = false;
+    public bool isLocked = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,15 +18,48 @@ public class InteractionSystemScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab) && journalOpen != true)
+        if (Input.GetKeyDown(KeyCode.E) && isPlayerNear == true)
         {
-            journalCanvas.SetActive(true);
-            journalOpen = true;
+            Debug.Log("Interacted");
+
+            if (isAbleToPickUp == true)
+            {
+                PickUpObject();
+                Debug.Log("Picked up object");
+            }
+            
+            if (isADoor == true && isLocked != true)
+            {
+                Debug.Log("Opened Door");
+            }
+            else if (isADoor == true && isLocked == true)
+            {
+                Debug.Log("");
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.Tab) && journalOpen == true)
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("TriggerOn");
+
+        if (collision.gameObject.tag == "Player")
         {
-            journalCanvas.SetActive(false);
-            journalOpen = false;
+            isPlayerNear = true;
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isPlayerNear = false;
+        }
+    }
+
+    void PickUpObject()
+    {
+        // Temporary Function
+        Destroy(gameObject);
     }
 }
---
+++ HorrorProjectProgramV2025/Assets/Scripts/JournalScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JournalScript : MonoBehaviour
+{
+    public GameObject journalCanvas;
+    bool journalOpen = false;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && journalOpen != true)
+        {
+            journalCanvas.SetActive(true);
+            journalOpen = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab) && journalOpen == true)
+        {
+            journalCanvas.SetActive(false);
+            journalOpen = false;
+        }
+    }
+}
