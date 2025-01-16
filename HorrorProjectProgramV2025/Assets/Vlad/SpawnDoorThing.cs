c8acb9f0daa75f0daea00cfae5e5d91a87203801
VladiBoy-netizen <vladislav.mashkov@elev.ga.lbs.se>
2025-01-16 16:20:37 +0100

    Update SpawnDoorThing.cs

--- HorrorProjectProgramV2025/Assets/Vlad/SpawnDoorThing.cs
+++ HorrorProjectProgramV2025/Assets/Vlad/SpawnDoorThing.cs
@@ -38,33 +38,47 @@ public class SpawnDoorThing : MonoBehaviour
         if (SceneID == 4 && f2)
         {
             int id = (int)Random.Range(0f, (float)SpawnPoints2.Count - 1f);
-            Door1Floor2.transform.position = SpawnPoints2[id];
-            SpawnPoints2.RemoveAt(id);
-
-            id = (int)Random.Range(0f, (float)SpawnPoints2.Count - 1f);
-            Door2Floor2.transform.position = SpawnPoints2[id];
-            SpawnPoints2.RemoveAt(id);
-
-            id = (int)Random.Range(0f, (float)SpawnPoints2.Count - 1f);
-            Door3Floor2.transform.position = SpawnPoints2[id];
-            SpawnPoints2.RemoveAt(id);
-
+            if (Door1Floor2 != null)
+            {
+                Door1Floor2.transform.position = SpawnPoints2[id];
+                SpawnPoints2.RemoveAt(id);
+            }
+            if (Door2Floor2 != null)
+            {
+                id = (int)Random.Range(0f, (float)SpawnPoints2.Count - 1f);
+                Door2Floor2.transform.position = SpawnPoints2[id];
+                SpawnPoints2.RemoveAt(id);
+            }
+            if (Door3Floor2 != null)
+            {
+                id = (int)Random.Range(0f, (float)SpawnPoints2.Count - 1f);
+                Door3Floor2.transform.position = SpawnPoints2[id];
+                SpawnPoints2.RemoveAt(id);
+            }
             f2 = false;
         }
 
         if (SceneID == 5 && f3)
         {
             int id = (int)Random.Range(0f, (float)SpawnPoints3.Count - 1f);
-            Door1Floor3.transform.position = SpawnPoints3[id];
-            SpawnPoints3.RemoveAt(id);
 
-            id = (int)Random.Range(0f, (float)SpawnPoints3.Count - 1f);
-            Door2Floor3.transform.position = SpawnPoints3[id];
-            SpawnPoints2.RemoveAt(id);
-
-            id = (int)Random.Range(0f, (float)SpawnPoints3.Count - 1f);
-            Door3Floor3.transform.position = SpawnPoints3[id];
-            SpawnPoints3.RemoveAt(id);
+            if (Door1Floor3 != null)
+            {
+                Door1Floor3.transform.position = SpawnPoints3[id];
+                SpawnPoints3.RemoveAt(id);
+            }
+            if (Door2Floor3 != null)
+            {
+                id = (int)Random.Range(0f, (float)SpawnPoints3.Count - 1f);
+                Door2Floor3.transform.position = SpawnPoints3[id];
+                SpawnPoints2.RemoveAt(id);
+            }
+            if (Door3Floor3 != null)
+            {
+                id = (int)Random.Range(0f, (float)SpawnPoints3.Count - 1f);
+                Door3Floor3.transform.position = SpawnPoints3[id];
+                SpawnPoints3.RemoveAt(id);
+            }
 
             f3 = false;
         }
