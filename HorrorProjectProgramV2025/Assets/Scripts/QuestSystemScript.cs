54eab5cf59a7fce433f428a2e89f62a1502ef988
VladiBoy-netizen <vladislav.mashkov@elev.ga.lbs.se>
2025-01-17 13:05:16 +0100

    Anyway, quest update n' Hallway2

--- HorrorProjectProgramV2025/Assets/Scripts/QuestSystemScript.cs
+++ HorrorProjectProgramV2025/Assets/Scripts/QuestSystemScript.cs
@@ -5,24 +5,12 @@ using UnityEngine;
 public class QuestSystemScript : MonoBehaviour
 {
     #region Quests
-    public GameObject quest1;
-    public GameObject quest2;
-    public GameObject quest3;
-    public GameObject quest4;
-    public GameObject quest5;
-    public GameObject quest6;
-    public GameObject quest7;
+    public List<GameObject> quests = new List<GameObject>();
 
     #endregion
 
     #region Checkmarks
-    public GameObject checkmark1;
-    public GameObject checkmark2;
-    public GameObject checkmark3;
-    public GameObject checkmark4;
-    public GameObject checkmark5;
-    public GameObject checkmark6;
-    public GameObject checkmark7;
+    public List<GameObject> checkMarks = new List<GameObject>();
 
     #endregion
 
@@ -46,40 +34,14 @@ public class QuestSystemScript : MonoBehaviour
     void UpdateJournal()
     {
         Debug.Log("Updated Journal");
-        if (JournalScript.questNumber == 2)
+        try
         {
-            quest2.SetActive(true);
-            checkmark2.SetActive(true);
+            quests[JournalScript.questNumber].gameObject.SetActive(true);
+            checkMarks[JournalScript.questNumber].gameObject.SetActive(true);
         }
-        else if (JournalScript.questNumber == 3)
+        catch
         {
-            quest3.SetActive(true);
-            checkmark3.SetActive(true);
-
-        }
-        else if (JournalScript.questNumber == 4)
-        {
-            quest4.SetActive(true);
-            checkmark4.SetActive(true);
-
-        }
-        else if (JournalScript.questNumber == 5)
-        {
-            quest5.SetActive(true);
-            checkmark5.SetActive(true);
-
-        }
-        else if (JournalScript.questNumber == 6)
-        {
-            quest6.SetActive(true);
-            checkmark6.SetActive(true);
-
-        }
-        else if (JournalScript.questNumber == 7)
-        {
-            quest7.SetActive(true);
-            checkmark7.SetActive(true);
-
+            print("Error to update Journal");
         }
     }
 }
